919f9071bd579d2982b17c4a215758ddc71ecd68
0u812 <0u812@users.noreply.github.com>
2017-09-17 22:48:31 -0700

    read time series data

--- B4/python/model.py
+++ B4/python/model.py
@@ -1,11 +1,35 @@
 from __future__ import division, print_function, absolute_import
 
-import tellurium as te, numpy as np, os
+import tellurium as te, numpy as np, os, csv
 
-with open(os.path.join(os.path.dirname(__file__), 'b4.sb') as f:
+# dir of script
+this_dir = os.path.dirname(__file__)
+
+with open(os.path.join(this_dir, 'b4.sb')) as f:
     model_str = f.read()
 
 # load the model into tellurium
 r = te.loada(model_str)
 
 # load the time series data
+rows = []
+with open(os.path.join(this_dir, 'data.csv'), 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    # get header row
+    for row in reader:
+        headers = row
+        break
+    for row in reader:
+        break # skip labels
+    for row in reader:
+        break # skip units
+    for row in reader:
+        np_row = np.array([float(n) for n in row])
+        rows.append(np_row)
+
+data = np.vstack(rows)
+print('Time-series observables')
+print(headers)
+print('Time-series data')
+print('*'*80)
+print(data)
